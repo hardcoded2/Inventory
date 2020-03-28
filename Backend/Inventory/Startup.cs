fbbda20a0812bf33da92dd8956f87fcf4e483cef
Alex Sink <alexsink@yahoo.com>
2020-03-27 20:02:57 -0700

    dinner iterating

--- Backend/Inventory/Startup.cs
+++ Backend/Inventory/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO.Pipelines;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Unicode;
 using System.Threading.Tasks;
 using Google.Protobuf;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -39,8 +42,7 @@ namespace WebApplication
                 endpoints.MapGet("/testrawstringattach", async context =>
                 {
                     var response = context.Response;
-                    await response.BodyWriter.WriteAsync(System.Text.Encoding.UTF8.GetBytes("HELLO WORLD"));
-                    Console.WriteLine("wrote text body");
+                    await SendString(response.BodyWriter,GetTestString());
                     
                     /*
                     //https://stackoverflow.com/questions/5895684/how-to-send-file-in-httpresponse not async though
@@ -76,8 +78,60 @@ namespace WebApplication
                     //await context.Response.WriteAsync(GetTestProto());
                 });
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/testrawstringheader", async context =>
+                {
+                    var response = context.Response;
+                    var multi = new MultipartContent();
+                    //var streamProvider = new FileMultipartSection(new MultipartSection(GetTestProtoBytes()));
+                    //await response.Headers. .BodyWriter.WriteAsync(GetTestProtoBytes());
+                    using (MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent())
+                    {
+                        //multipartFormDataContent.Add(new ByteArrayContent(GetTestProtoBytes()),"File");
+                        multipartFormDataContent.Add(new StringContent(GetTestString()),"File");
+                        response.BodyWriter.WriteAsync(multipartFormDataContent);
+                        /*
+                        multipartFormDataContent.Add(new StringContent(JsonConvert.SerializeObject(myFile), Encoding.UTF8, "application/json"), nameof(MyFile));
+
+                        HttpResponseMessage httpResult = await httpClient.PostAsync("api/uploaddownload/upload", multipartFormDataContent).ConfigureAwait(false);
+
+                        httpResult.EnsureSuccessStatusCode();
+                        stream = await httpResult.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                        */
+                    }
+                    
+                    /*
+                    //https://stackoverflow.com/questions/5895684/how-to-send-file-in-httpresponse not async though
+                    response.Clear();
+                    const string SaveAsFileName = "TestProto";
+                    response.Headers.Add("content-disposition", "attachment;filename=" + SaveAsFileName);
+                    response.ContentType = "application/octet-stream";
+                    response.BinaryWrite(GetTestProto());
+                    response.End();
+                    */
+                    //await context.Response
+                    //await context.Response.WriteAsync(GetTestProto());
+                });
+            });
         }
 
+        private async Task SendString(PipeWriter writer, string str)
+        {
+            var strBytes = System.Text.Encoding.UTF8.GetBytes(str);
+            await writer.WriteAsync(strBytes);
+            Console.WriteLine($"wrote text body {str}");
+        }
+        private async Task SendStringAsAttachment(HttpContext context, string str)
+        {
+            var strBytes = System.Text.Encoding.UTF8.GetBytes(str);
+            await writer.WriteAsync(strBytes);
+            Console.WriteLine($"wrote text body {str}");
+        }
+        public static string GetTestString()
+        {
+            return "HELLO WORLD";
+        }
         public static PlayerData GetTestProto()
         {
             return new PlayerData(){DataVersion = 100};
